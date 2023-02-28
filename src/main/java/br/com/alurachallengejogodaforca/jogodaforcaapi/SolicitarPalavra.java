a34e3a43a51181b4822573028f39ccc02cecace0
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-02-28 17:06:14 -0300

    trocando o requestparam por requstbody

--- src/main/java/br/com/alurachallengejogodaforca/jogodaforcaapi/SolicitarPalavra.java
+++ src/main/java/br/com/alurachallengejogodaforca/jogodaforcaapi/SolicitarPalavra.java
@@ -3,11 +3,11 @@ package br.com.alurachallengejogodaforca.jogodaforcaapi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.alurachallengejogodaforca.jogodaforcaapi.factory.ConnectionFactory;
 
 import br.com.alurachallengejogodaforca.jogodaforcaapi.controller.SolicitarController;
+import br.com.alurachallengejogodaforca.jogodaforcaapi.factory.ConnectionFactory;
 
 @RestController
 public class SolicitarPalavra {
@@ -23,7 +23,7 @@ public class SolicitarPalavra {
 	
 	@CrossOrigin
 	@PostMapping("/solicitar")
-	public void solicitar(@RequestParam(value = "palavra", defaultValue="") String palavra) {
+	public void solicitar(@RequestBody String palavra) {
 		
 		if(palavra == "" || palavra.length() < 3 || !palavra.matches("[a-z]+")) return;
 		
